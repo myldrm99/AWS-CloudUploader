f52356dc43f553c2be3f801932f1fbcc6b023c7c
myldrm99 <48071090+myldrm99@users.noreply.github.com>
2024-04-22 20:34:21 +0300

    Update install_awsclouduploader.sh

--- install_awsclouduploader.sh
+++ install_awsclouduploader.sh
@@ -3,25 +3,11 @@
 # Set the installation directory
 INSTALL_DIR="/usr/local/bin"
 
-# Specify the name of your CloudUploader script
-CLOUD_UPLOADER_SCRIPT="awsclouduploader.sh"
-
-# Check if the installation directory exists
-if [ ! -d "$INSTALL_DIR" ]; then
-    echo "Error: Installation directory $INSTALL_DIR does not exist."
-    exit 1
-fi
-
-# Copy the CloudUploader script to the installation directory
-cp "$CLOUD_UPLOADER_SCRIPT" "$INSTALL_DIR"
+# Copy the main script to the installation directory
+cp awsclouduploader.sh $INSTALL_DIR
 
 # Provide execute permissions
-chmod +x "$INSTALL_DIR/$CLOUD_UPLOADER_SCRIPT"
+chmod +x $INSTALL_DIR/awsclouduploader.sh
 
-# Check if the copy operation was successful
-if [ $? -eq 0 ]; then
-    echo "CloudUploader is now installed in $INSTALL_DIR."
-    echo "You can use it by running '$CLOUD_UPLOADER_SCRIPT'."
-else
-    echo "Error: Failed to install CloudUploader."
-fi
+echo "AWS CloudUploader is now installed."
+echo "You can use it by running 'awsclouduploader.sh' ."
